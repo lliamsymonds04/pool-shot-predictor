02b9b3f4196ac109a9f9289baf3b23315fb00620
lts-pj <lliamsymonds04@gmail.com>
2025-03-15 17:09:54 +1000

    added table warping

--- TableHandler.py
+++ TableHandler.py
@@ -55,22 +55,34 @@ def detect_pool_table(image_path: str, debug: bool = False):
         # cv2.drawContours(touchedup_image, [approx], -1, (0, 0, 255), 10)
         
         #sort the points
-        # Sort by Y first (top 2 points, bottom 2 points)
-        approx = sorted(approx, key=lambda p: p[0][1])
+        approx = sorted(approx, key=lambda p: p[0][1]) # Sort by y-coordinate
 
         # Now sort left-right for the top and bottom pairs
         top_points = sorted(approx[:2], key=lambda p: p[0][0])  # Top-left, top-right
         bottom_points = sorted(approx[2:], key=lambda p: p[0][0])  # Bottom-left, bottom-right
 
-        ordered_corners = np.array([top_points[0], top_points[1], bottom_points[1], bottom_points[0]])
+        #want to get the ratio of distance between points to determine which way is the longer side
+        top_len = np.linalg.norm(top_points[0] - top_points[1])
+        side_len = np.linalg.norm(bottom_points[0] - top_points[0])
+        if top_len > side_len:
+            ordered_corners = np.array([top_points[0], top_points[1], bottom_points[1], bottom_points[0]])
+        else:
+            ordered_corners = np.array([top_points[1], bottom_points[1], bottom_points[0], top_points[0]])
 
-        cv2.drawContours(touchedup_image, [ordered_corners.astype(int)], -1, (255, 0, 255), 10)
-        cv2.circle(touchedup_image, (ordered_corners[0][0]).astype(int), 40, (0, 0, 255), -1)
-        
         #warp the image to a top down view
+        table_length = 800
+        table_width = 400
+        dst = np.array([[0, 0], [table_length, 0], [table_length, table_width], [0, table_width]], np.float32)
+        
+        matrix = cv2.getPerspectiveTransform(ordered_corners.astype(np.float32), dst)
+        warped = cv2.warpPerspective(touchedup_image, matrix, (table_length, table_width))
         
         #find the balls
         
+        #draw the contour
+        cv2.drawContours(touchedup_image, [ordered_corners.astype(int)], -1, (255, 0, 255), 10)
+        cv2.circle(touchedup_image, (ordered_corners[0][0]).astype(int), 40, (0, 0, 255), -1)
+
         #scale the final image
         w = 800
         old_w = touchedup_image.shape[1]
@@ -81,6 +93,7 @@ def detect_pool_table(image_path: str, debug: bool = False):
 
         #display the image
         cv2.imshow("Table", final_image)
+        cv2.imshow("Warped", warped)
         if cv2.waitKey(0) == ord('q'):
             cv2.destroyAllWindows()
 
@@ -89,7 +102,8 @@ def detect_pool_table(image_path: str, debug: bool = False):
 
 
 if __name__ == "__main__":
-    image_name = "broken_1"
+    # image_name = "broken_1"
+    image_name = "broken_4"
     image_path = f"images/table/{image_name}.jpg"
 
     detect_pool_table(image_path)
