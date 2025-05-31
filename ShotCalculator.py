6ae4a1b6b25f57f00f291841ed148d8006e46352
lts-pj <lliamsymonds04@gmail.com>
2025-05-31 15:34:07 +1000

    added ghost ball calculator and pocket positions

--- ShotCalculator.py
+++ ShotCalculator.py
@@ -4,14 +4,24 @@ import numpy as np
 from MyTypes import BallClassification
 from ProcessBalls import colours
 
+def get_ghost_ball_position(ball_x: int, ball_y: int, pocket_x: int, pocket_y: int, white_ball_radius: int) -> tuple[int, int]:
+    ball_to_pocket = np.array([pocket_x - ball_x, pocket_y - ball_y])
+    ball_to_pocket_length = np.linalg.norm(ball_to_pocket)
+    if ball_to_pocket_length == 0:
+        return (ball_x, ball_y)  # Avoid division by zero
 
+    direction = ball_to_pocket / ball_to_pocket_length
+    ghost_ball_x = int(pocket_x - direction[0] * white_ball_radius)
+    ghost_ball_y = int(pocket_y - direction[1] * white_ball_radius)
+
+    return (ghost_ball_x, ghost_ball_y)
+    
 
 def calculate_best_shot(table: np.ndarray, balls: list[tuple[int]], ball_classifications: list[BallClassification]):
-
-    # For now, just print the positions and classifications
     print("Calculating best shot...")
+
+    #draw the balls
     for ball, classification in zip(balls, ball_classifications):
-        print(classification.colour)
         x, y, radius = ball
         hsv = colours[classification.colour]
         b, g, r = cv2.cvtColor(np.uint8([[hsv]]), cv2.COLOR_HSV2BGR)[0][0]
@@ -20,6 +30,36 @@ def calculate_best_shot(table: np.ndarray, balls: list[tuple[int]], ball_classif
         
         cv2.circle(table, (x, y), radius, (b,g,r), 2)
 
+    #find the white ball
+    white_ball_index = -1
+    for ball, classification in zip(balls, ball_classifications):
+        if classification.colour == "white":
+            white_ball_index = balls.index(ball)
+            break
+
+    if white_ball_index == -1:
+        print("No white ball found!")
+        return
+
+    #create the pocket positions
+    table_width, table_length = table.shape[:2]
+    pocket_positions = [
+        (0,0), #top left
+        (int(table_length / 2), 0), #top middle
+        (table_length, 0), #top right
+        (0, table_width), #bottom left
+        (int(table_length / 2), table_width), #bottom middle
+        (table_length, table_width) #bottom right
+    ]
+
+    #calculate the best shot
+    white_ball_x, white_ball_y, _ = balls[white_ball_index]
+    for pocket in pocket_positions:
+        pocket_x, pocket_y = pocket
+
+
+        
+    
+    
     cv2.imshow("Best Shot", table)
     
-    #draw all the balls on the table
