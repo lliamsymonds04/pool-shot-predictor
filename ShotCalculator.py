fixed stripped ball shot calculation

ShotCalculator.py
@@ -137,9 +137,11 @@ def calculate_best_shot(table: np.ndarray, balls: list[tuple[int]], ball_classif
     possible_balls = []
     found_black = -1
     for i, classification in enumerate(ball_classifications):
-        if classification.colour == "black":
+        if i == "unknown":
+            continue
+        elif classification.colour == "black":
             found_black = True
-        elif classification.colour != "white" and (not stripped or classification.stripped):
+        elif classification.colour != "white" and (classification.stripped == stripped):
             possible_balls.append(i)
 
     if len(possible_balls) == 0:

main.py
@@ -24,13 +24,9 @@ def process_image(img: np.ndarray):
     merged_balls = merge_balls(balls, balls2)
 
     ball_classifications = classify_balls(merged_balls, removed_green)
-    #result = draw_balls_classificiation(table, merged_balls, ball_classifications)
-    #cv2.imshow("Classified Balls", result)
-    #print(f"classified ball as {debug_classify_ball(merged_balls, removed_green, 4)}");
-
     raw_table = warp_table(img, warp_matrix, 1000)
 
-    calculate_best_shot(raw_table, merged_balls, ball_classifications)
+    calculate_best_shot(raw_table, merged_balls, ball_classifications, False)
     
     if cv2.waitKey(0) == ord('q'):
         cv2.destroyAllWindows()